e46521fc6f32ccfc5dbb095a2a75b3c66fbc9815
Tinusw <tinus@3months.com>
2018-04-12 22:25:19 +0200

    mongoDB up and running via moongoose

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const mongoose = require('mongoose');
+
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
-const http = require("http");
-const app = require('./app');
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`OH NO → ${err.message}`);
+});
 
 // Server setup
+const app = require('./app');
 const port = process.env.PORT || 3030;
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
