f6a762e294a5c8423663021bd1644663b79a8ba9
Tinusw <tinus@3months.com>
2018-04-13 00:01:49 +0200

    adding bcrypt and before save hook to hash user password

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // ensure mongoose uses native promises
 mongoose.Promise = global.Promise;
+const bcrypt = require("bcrypt-nodejs");
 const validator = require("validator");
 const mongodbErrorHandler = require("mongoose-mongodb-errors");
 
@@ -16,7 +17,31 @@ const userSchema = new Schema({
     lowercase: true,
     trim: true,
     required: "Please use a valid email address"
+  },
+  password: {
+    type: String,
+    required: "Please supply a password"
   }
+});
+
+// Pre-save hook to encrypt password with bcrypt
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // connect mongodb Error Handler
