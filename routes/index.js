87d423722431517ab9cb40682cd940e89b02ca62
Tinusw <tinus.wagnersa@gmail.com>
2018-04-14 15:58:42 +0200

    added JWT token auth, testing on root path

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const { catchErrors } = require("../handlers/errorHandlers");
+const passportService = require("../services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+router.get('/', requireAuth, function(req, res) {
+  res.send({ hi: 'there'})
+});
 
 router.post("/signup", catchErrors(authController.signup));
 
